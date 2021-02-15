2e8e7c6e940f458348aca41cc9dc71e32ffca1f9
Oliver Heimlich <oheim@posteo.de>
2021-02-15 21:00:38 +0100

    tuya-cover.py: Improve error handling for background jobs

--- tuya-cover.py
+++ tuya-cover.py
@@ -48,17 +48,22 @@ def update_device_status():
     device = tinytuya.CoverDevice(config['DEVICE_ID'], hostname, config['LOCAL_KEY'])
     device.set_version(3.3)
     device.set_socketRetryLimit(120)
-    status = device.status()
-    if status['dps']['1'] == 'close':
-        if not is_closed:
-            logging.info('Markise wird manuell geschlossen')
-            is_closed = True
-    elif status['dps']['1'] == 'open':
-        if is_closed:
-            logging.info('Markise wird manuell geöffnet')
-            is_closed = False
-    # else:
-        #unknown
+    
+    try:
+        status = device.status()
+        if status['dps']['1'] == 'close':
+            if not is_closed:
+                logging.info('Markise wird manuell geschlossen')
+                is_closed = True
+        elif status['dps']['1'] == 'open':
+            if is_closed:
+                logging.info('Markise wird manuell geöffnet')
+                is_closed = False
+        # else:
+            #unknown
+    except:
+        logging.exception('Fehler beim Abruf des IoT-Device-Status')
+            
 
 schedule = None
 @background.job(interval = datetime.timedelta(minutes = 15))
@@ -68,54 +73,59 @@ def update_schedule():
     
     logging.info('Aktualisiere Wettervorhersage ...')
     
-    local_stations = DWDMosmixStations().nearby_number(float(config['LATITUDE']), float(config['LONGITUDE']), 5)
+    try:
+        local_stations = DWDMosmixStations().nearby_number(float(config['LATITUDE']), float(config['LONGITUDE']), 5)
+        
+        parameters = [
+                DWDMosmixParameter.PROBABILITY_PRECIPITATION_GT_0_1_MM_LAST_1H,
+                DWDMosmixParameter.WIND_GUST_MAX_LAST_1H,
+                DWDMosmixParameter.SUNSHINE_DURATION,
+                DWDMosmixParameter.TEMPERATURE_DEW_POINT_200,
+                DWDMosmixParameter.TEMPERATURE_AIR_200]
+        
+        mosmix = wetterdienst.dwd.forecasts.DWDMosmixValues(
+                station_id = local_stations['STATION_ID'].to_list(),
+                mosmix_type = wetterdienst.dwd.forecasts.DWDMosmixType.LARGE,
+                start_issue = wetterdienst.dwd.forecasts.metadata.dates.DWDForecastDate.LATEST,
+                parameter = parameters
+                )
+        
+        # Read data
+        forecast = mosmix.all().dropna().pivot(
+                index = ['DATE', 'STATION_ID'],
+                columns = 'PARAMETER',
+                values = 'VALUE')
+        
+        # Aggregate over all stations (use pessimistic values)
+        forecast_agg = forecast.groupby('DATE').agg({
+                DWDMosmixParameter.PROBABILITY_PRECIPITATION_GT_0_1_MM_LAST_1H.value: 'max',
+                DWDMosmixParameter.WIND_GUST_MAX_LAST_1H.value: 'max',
+                DWDMosmixParameter.SUNSHINE_DURATION.value: 'max',
+                DWDMosmixParameter.TEMPERATURE_DEW_POINT_200.value: 'max',
+                DWDMosmixParameter.TEMPERATURE_AIR_200.value: 'min'
+                })
+        
+        # Default = leave open
+        local_schedule = DataFrame(False, index = forecast_agg.index, columns=['CLOSE'])
+        # Close, if more than 5 Minutes sunshine per hour
+        local_schedule[forecast_agg[DWDMosmixParameter.SUNSHINE_DURATION.value] > 5 * 60] = True
+        # Open, if windy
+        local_schedule[forecast_agg[DWDMosmixParameter.WIND_GUST_MAX_LAST_1H.value] > 10] = False
+        # Open, if rainy
+        local_schedule[forecast_agg[DWDMosmixParameter.PROBABILITY_PRECIPITATION_GT_0_1_MM_LAST_1H.value] > 50.0] = False
+        # Open, if below 4°C to protect from ice and snow
+        local_schedule[forecast_agg[DWDMosmixParameter.TEMPERATURE_AIR_200.value] < 277.15] = False
+        # Open, if below dew point to protect from moisture
+        local_schedule[forecast_agg[DWDMosmixParameter.TEMPERATURE_DEW_POINT_200.value] > forecast_agg[DWDMosmixParameter.TEMPERATURE_AIR_200.value]] = False
+        
+        # Update global variable
+        schedule = local_schedule
     
-    parameters = [
-            DWDMosmixParameter.PROBABILITY_PRECIPITATION_GT_0_1_MM_LAST_1H,
-            DWDMosmixParameter.WIND_GUST_MAX_LAST_1H,
-            DWDMosmixParameter.SUNSHINE_DURATION,
-            DWDMosmixParameter.TEMPERATURE_DEW_POINT_200,
-            DWDMosmixParameter.TEMPERATURE_AIR_200]
-    
-    mosmix = wetterdienst.dwd.forecasts.DWDMosmixValues(
-            station_id = local_stations['STATION_ID'].to_list(),
-            mosmix_type = wetterdienst.dwd.forecasts.DWDMosmixType.LARGE,
-            start_issue = wetterdienst.dwd.forecasts.metadata.dates.DWDForecastDate.LATEST,
-            parameter = parameters
-            )
-    
-    # Read data
-    forecast = mosmix.all().dropna().pivot(
-            index = ['DATE', 'STATION_ID'],
-            columns = 'PARAMETER',
-            values = 'VALUE')
-    
-    # Aggregate over all stations (use pessimistic values)
-    forecast_agg = forecast.groupby('DATE').agg({
-            DWDMosmixParameter.PROBABILITY_PRECIPITATION_GT_0_1_MM_LAST_1H.value: 'max',
-            DWDMosmixParameter.WIND_GUST_MAX_LAST_1H.value: 'max',
-            DWDMosmixParameter.SUNSHINE_DURATION.value: 'max',
-            DWDMosmixParameter.TEMPERATURE_DEW_POINT_200.value: 'max',
-            DWDMosmixParameter.TEMPERATURE_AIR_200.value: 'min'
-            })
-    
-    # Default = leave open
-    local_schedule = DataFrame(False, index = forecast_agg.index, columns=['CLOSE'])
-    # Close, if more than 5 Minutes sunshine per hour
-    local_schedule[forecast_agg[DWDMosmixParameter.SUNSHINE_DURATION.value] > 5 * 60] = True
-    # Open, if windy
-    local_schedule[forecast_agg[DWDMosmixParameter.WIND_GUST_MAX_LAST_1H.value] > 10] = False
-    # Open, if rainy
-    local_schedule[forecast_agg[DWDMosmixParameter.PROBABILITY_PRECIPITATION_GT_0_1_MM_LAST_1H.value] > 50.0] = False
-    # Open, if below 4°C to protect from ice and snow
-    local_schedule[forecast_agg[DWDMosmixParameter.TEMPERATURE_AIR_200.value] < 277.15] = False
-    # Open, if below dew point to protect from moisture
-    local_schedule[forecast_agg[DWDMosmixParameter.TEMPERATURE_DEW_POINT_200.value] > forecast_agg[DWDMosmixParameter.TEMPERATURE_AIR_200.value]] = False
-    
-    # Update global variable
-    schedule = local_schedule
+        logging.info('Wettervorhersage aktualisiert')
+        
+    except:
+        logging.exception('Fehler beim Abruf der Wetterdaten')
 
-    logging.info('Wettervorhersage aktualisiert')
 
 @background.job(interval = datetime.timedelta(minutes = 1))
 def apply_schedule():
@@ -127,27 +137,31 @@ def apply_schedule():
     device = tinytuya.CoverDevice(config['DEVICE_ID'], hostname, config['LOCAL_KEY'])
     device.set_version(3.3)
     device.set_socketRetryLimit(5)
-    status = device.status()
-    if status['dps']['1'] != 'stop':
-        # Do nothing if the device is operating right now
-        return
 
-    now = datetime.datetime.now(datetime.timezone.utc).astimezone()
-    close_now = schedule[schedule.index.to_pydatetime() > now]['CLOSE'].iloc[0]
-    if is_closed == close_now:
-        # Nothing to do
-        return
+    try:
+        now = datetime.datetime.now(datetime.timezone.utc).astimezone()
+        close_now = schedule[schedule.index.to_pydatetime() > now]['CLOSE'].iloc[0]
+        if is_closed == close_now:
+            # Nothing to do
+            return
 
-    if close_now:
-        if not (is_closed is None):
-            bot_send('Markise wird automatisch geschlossen')
-        device.set_value(1, 'close')
-    else:
-        if not (is_closed is None):
-            bot_send('Markise wird automatisch geöffnet')
-        device.set_value(1, 'open')
-    is_closed = close_now
-
+        status = device.status()
+        if status['dps']['1'] != 'stop':
+            # Do nothing if the device is operating right now
+            return
+    
+        if close_now:
+            if not (is_closed is None):
+                bot_send('Markise wird automatisch geschlossen')
+            device.set_value(1, 'close')
+        else:
+            if not (is_closed is None):
+                bot_send('Markise wird automatisch geöffnet')
+            device.set_value(1, 'open')
+        is_closed = close_now
+        
+    except:
+        logging.exception('Fehler beim Anwenden des Plans')        
 
 update_schedule()
 
